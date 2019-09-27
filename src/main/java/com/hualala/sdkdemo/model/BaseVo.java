cf84377878b7177fa0839c821fa154687d516a67
Always、 <2199382542.com>
2019-09-27 22:37:52 +0800

    加上作者信息

--- src/main/java/com/hualala/sdkdemo/model/BaseVo.java
+++ src/main/java/com/hualala/sdkdemo/model/BaseVo.java
@@ -2,6 +2,12 @@ package com.hualala.sdkdemo.model;
 
 import lombok.Data;
 
+/**
+ * @Author: GH
+ * @Date: 2019/9/27 22:03
+ * @Version 1.0
+ * 请求的商品Id 和组Id
+ */
 @Data
 public class BaseVo {
     private Long shopID;
